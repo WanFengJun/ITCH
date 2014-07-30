e656cb22eb7637e7a38f51689136a3865309e014
jinming626 <onlinestudio626@gmail.com>
2014-07-30 02:53:17 -0700

    not yet ready.

---
+++ FluffyITCHTests/FluffyITCHTests.m
@@ -0,0 +1,34 @@
+//
+//  FluffyITCHTests.m
+//  FluffyITCHTests
+//
+//  Created by Mimi on 7/2/14.
+//  Copyright (c) 2014 Jin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FluffyITCHTests : XCTestCase
+
+@end
+
+@implementation FluffyITCHTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
